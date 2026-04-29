9e7989b08e9ea48953ddf3b314232783f7c6a0cb
ewbankleonardo <ewbankv.leonardo@gmail.com>
2026-04-29 16:19:19 -0300

    numero de caracteres

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Primeiro código")
-print ("Olá" + " 3 4 5")
-a = input("Digite algo")
-print (a)
+#print("Primeiro código")
+#print ("Olá" + " 3 4 5")
+#a = input("Digite algo")
+#print (a)
+
+
+##Numero de caracteres em um nome 
+a=input("Digite seu nome")
+print(len(a))
